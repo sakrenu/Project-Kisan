b7def172015d813387b683927d1097724675907b
sakrenu <renukasakshivpatil@gmail.com>
2025-07-24 04:53:17 +0530

    bg img added to dashboard

--- client/src/app/dashboard/page.js
+++ client/src/app/dashboard/page.js
@@ -8,15 +8,19 @@ import VoiceFirstBanner from '@/components/VoiceFirstBanner';
 import ServiceGrid from '@/components/ServiceGrid';
 import Footer from '@/components/Footer';
 import { useTranslation } from 'react-i18next';
+import styles from './dashboard.module.css';
 
 export default function Dashboard() {
 //   const { t } = useLanguage();
 const { t } = useTranslation();
 
+
   return (
-    <div className="dashboard-container">
+    <div className={styles.dashboardContainer}>
+
       <Navbar />
-      <main className="dashboard-main">
+      <main className={styles.dashboardMain}>
+
         <WelcomeBanner t={t} />
         <AIAssistantCard t={t} />
         <QuickActions t={t} />
